8c0ce837e87115346841c11be6c67b908b42749c
Wintermourn <gladosnwheatley@gmail.com>
2025-04-12 22:06:27 -0500

    OSK: modify switchcaser, support default case

--- OnScreenKeyboard/Data/Script/screenkeyboard/lib/switchcaser.lua
+++ OnScreenKeyboard/Data/Script/screenkeyboard/lib/switchcaser.lua
@@ -3,16 +3,16 @@
 ---@overload fun(self: table, case: any, ...): ...
 
 ---@param table {[any]: fun(...): ...}
+---@param default fun(...)? fallback function if a requested case isn't found
 ---@return Switch
-return function (table)
+return function (table, default)
     ---@overload fun(self: table, case: any, ...): ...
     return setmetatable(table, {
-        __index = function (t, k)
-            return nil;
-        end,
         __call = function (t, case, ...)
             if t[case] then
                 return t[case](...);
+            elseif default then
+                return default(...);
             end
         end
     });
